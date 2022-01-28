365e785ca6044f8662d08785aaa6450425b2ebb7
Veronika Sirak <7nicka@gmail.com>
2022-01-28 23:26:10 +0000

    add html and css
    add html and css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-$(function(){
+$(function () {
+  $(".hero-slider").slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="/images/prev.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="/images/next.svg" alt=""></button>',
+    vertical: true,
+  });
 
+  $(".product-name").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    centerMode: true,
+    asNavFor: ".product-content",
+    vertical: true,
+    prevArrow:
+      '<button type="button" class="product-prev"><img src="/images/product-prev.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="product-next"><img src="/images/product-next.svg" alt=""></button>',
+    vertical: true,
+  });
+
+  $(".product-content").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".product-name",
+    fade: true,
+    arrows: false,
+  });
+
+  $(".menu-btn").on("click", function () {
+    $(".menu-list").toggleClass("menu-active");
+  });
 });
